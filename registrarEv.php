3fbc1648f3fe54814e275e4e2c1847b322ea6d2a
GUILLERMO\Ana Guillermo <saori_bcht@hotmail.com>
2017-06-27 11:46:09 -0500

    muestra todo los registros

--- registrarEv.php
+++ registrarEv.php
@@ -3,8 +3,10 @@ include 'conexion.php';
 $nombre = $_POST ["nombre"];
 $correo = $_POST["correo"];
 $numTelefonico = $_POST["numTelefonico"];
-
-$insert =  "INSERT INTO  usuario (nombre, correo, numTelefonico) VALUES ('$nombre', '$correo', '$numTelefonico')";
+$especialidad = $_POST["especialidad"];
+$titulo = $_POST["titulo"];
+$institucion = $_["institucion"];
+$insert =  "INSERT INTO  usuario (nombre, correo, numTelefonico, especialidad, titulo, institucion) VALUES ('$nombre', '$correo', '$numTelefonico', '$especialidad', '$titulo', '$institucion')";
 //ejecutar consulta
 $resultado = mysqli_query ($conexion, $insert);
 if(!$resultado){
