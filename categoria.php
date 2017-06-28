e051b55af21f422f78eff220265385035144f1a1
hpplus\hp <SarahiLopezCrz@hotmail.com>
2017-06-27 19:10:49 -0500

    si inserta pero solo tabla por tabla

--- categoria.php
+++ categoria.php
@@ -1,27 +1,32 @@
-?><?php
-include 'conexion.php'
+<?php
+include 'conexion.php';
 
-  mysql_connect("localhost"," ","root"); 
-
-//selección de la base de datos con la que vamos a trabajar 
-mysql_select_db("verano1"); 
-
-  $nombrePro = $_POST["nombrePro"]
-  $Categoria=$_POST["categoria"];
-  $ficha=$_POST["fichaTecnica"]
-
-  $insert = " INSERT INTO proyecto (nombrePro,Categoria,fichaTecnica) VALUES ('$nombrePro', '$categoria', '$fichaTecnica')"
-
-  $resultado = mysql_query($conexion, $insert);
-  if(!$resultado){
-
-  	echo "Error";
-  }else{
-  	echo "registrado";
-  }
- 
+  $nombrePro = $_POST["nombrePro"];
+  $categoria=$_POST["categoria"];
+  $fichaTecnica=$_POST["fichaTecnica"];
+ $nombreInt=$_POST["nombreInt"];
+  $ApellidoPaternoInt=$_POST["ApellidoPaternoInt"];
+  $ApellidoMaternoInt=$_POST["ApellidoMaternoInt"];
+  $NombreAs=$_POST["NombreAs"];
+  $ApellidoPaternoAS=$_POST["ApellidoPaternoAS"];
+  $ApellidoMaternoAS=$_POST["ApellidoMaternoAS"];
   
 
+
+
+
+  $insertar =  "INSERT INTO  proyecto (nombrePro,categoria,fichaTecnica) VALUES ('$nombrePro', '$categoria', '$fichaTecnica')"; 
+  //mysql_query($insertar, $conexion);
+  $inserttres =  "INSERT INTO asesor(NombreAs,ApellidoPaternoAS,ApellidoMaternoAS) VALUES ('$NombreAs', '$ApellidoPaternoAS', '$ApellidoMaternoAS')"; 
+  //mysql_query($inserttres, $conexion);
+  $insertdos = "INSERT INTO integrantes (nombreInt,ApellidoPaternoInt,ApellidoMaternoInt) VALUES ('$nombreInt', '$ApellidoPaternoInt', '$ApellidoMaternoInt')";
+  
+
+
+  $resultado = mysqli_query ($conexion, $inserttres);
+if(!$resultado){
+  echo 'Error al registrar';
+}else{
+  echo 'Evaluador Registrado';
+}
  
- 
-?>	 
