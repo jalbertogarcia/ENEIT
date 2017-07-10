e6f25db7221834c7e548772f88d7e44807e57719
hpplus\hp <SarahiLopezCrz@hotmail.com>
2017-07-10 10:09:12 -0500

    xfi yaa

--- categoria.php
+++ categoria.php
@@ -9,12 +9,48 @@ if (isset($_POST['subir'])){
    $nombrePro = $_POST["nombrePro"];
   $categoria=$_POST["categoria"];
   $fichaTecnica=$_FILES["fichaTecnica"];
+  //intgrante1
   $nombreInt=$_POST["nombreInt"];
   $ApellidoPaternoInt=$_POST["ApellidoPaternoInt"];
   $ApellidoMaternoInt=$_POST["ApellidoMaternoInt"];
+  //Asesor1
   $NombreAs=$_POST["NombreAs"];
   $ApellidoPaternoAS=$_POST["ApellidoPaternoAS"];
   $ApellidoMaternoAS=$_POST["ApellidoMaternoAS"];
+  //Asesor2
+  $NombreAs2=$_POST["NombreAs"];
+  $ApellidoPaternoAS2=$_POST["ApellidoPaternoAS"];
+  $ApellidoMaternoAS2=$_POST["ApellidoMaternoAS"];
+  //Asesor3
+  $NombreAs3=$_POST["NombreAs"];
+  $ApellidoPaternoAS3=$_POST["ApellidoPaternoAS"];
+  $ApellidoMaternoAS3=$_POST["ApellidoMaternoAS"];
+
+
+
+
+
+//integrante2
+
+ $nombreInt2=$_POST["nombreInt"];
+  $ApellidoPaternoInt2=$_POST["ApellidoPaternoInt"];
+  $ApellidoMaternoInt2=$_POST["ApellidoMaternoInt"];
+
+//integrante3
+  $nombreInt3=$_POST["nombreInt"];
+  $ApellidoPaternoInt3=$_POST["ApellidoPaternoInt"];
+  $ApellidoMaternoInt3=$_POST["ApellidoMaternoInt"];
+//integrante4
+  $nombreInt4=$_POST["nombreInt"];
+  $ApellidoPaternoInt4=$_POST["ApellidoPaternoInt"];
+  $ApellidoMaternoInt4=$_POST["ApellidoMaternoInt"];
+//integrante5
+  $nombreInt5=$_POST["nombreInt"];
+  $ApellidoPaternoInt5=$_POST["ApellidoPaternoInt"];
+  $ApellidoMaternoInt5=$_POST["ApellidoMaternoInt"];
+
+
+
 
 
 $nombre = $_FILES['fichaTecnica']['name'];
@@ -36,20 +72,31 @@ $insertar =  "INSERT INTO  proyecto (nombrePro,categoria) VALUES ('$nombrePro', 
    $resu = mysqli_query($conexion, $insertar);
   
  
-  $inserttres =  "INSERT INTO asesor(NombreAs,ApellidoPaternoAS,ApellidoMaternoAS) VALUES ('$NombreAs', '$ApellidoPaternoAS', '$ApellidoMaternoAS')"; 
+  $inserttres =  "INSERT INTO asesor(NombreAs,ApellidoPaternoAS,ApellidoMaternoAS)
+   VALUES 
+   ('$NombreAs', '$ApellidoPaternoAS', '$ApellidoMaternoAS'),
+    ('$NombreAs2', '$ApellidoPaternoAS2', '$ApellidoMaternoAS2'),
+     ('$NombreAs3', '$ApellidoPaternoAS3', '$ApellidoMaternoAS3')"; 
   $resultadoses = mysqli_query ($conexion, $inserttres);
 
 
-
-
-
-
-  $insertdos = "INSERT INTO integrantes (nombreInt,ApellidoPaternoInt,ApellidoMaternoInt) VALUES ('$nombreInt', '$ApellidoPaternoInt', '$ApellidoMaternoInt')";
+  $insertdos = "INSERT INTO integrantes (nombreInt,ApellidoPaternoInt,ApellidoMaternoInt) 
+  VALUES
+   ('$nombreInt', '$ApellidoPaternoInt', '$ApellidoMaternoInt'),
+   ('$nombreInt2', '$ApellidoPaternoInt2','$ApellidoMaternoInt2'),
+   ('$nombreInt3', '$ApellidoPaternoInt3','$ApellidoMaternoInt3'),
+   ('$nombreInt4', '$ApellidoPaternoInt4','$ApellidoMaternoInt4'),
+    ('$nombreInt5', '$ApellidoPaternoInt5','$ApellidoMaternoInt5')";
+  
   $resultado = mysqli_query ($conexion, $insertdos);
 
+   
+ 
 
 
-if(!$resultado&!$resultadoses&!$resu){
+
+
+if(!$resultadoses&!$resu&!$resultado){
   echo 'Error al registrar';
 }else{
   echo 'Registro correctoo';
