0f400a5f504ea44236c836f6c644b8eb0f08b76e
hpplus\hp <SarahiLopezCrz@hotmail.com>
2017-07-11 12:22:40 -0500

    final

--- rubi.php
+++ rubi.php
@@ -4,13 +4,14 @@ include 'config.inc.php';
 
 if (isset($_POST['subir'])){
 
-
+$formato = array('.pdf');
 $documentacion = $_FILES['rubricass']['name'];
     $tipo = $_FILES['rubricass']['type'];
     $tamano = $_FILES['rubricass']['size'];
     $ruta = $_FILES['rubricass']['tmp_name'];
-    $destino = "Rubricas/".$documentacion;
-   
+    $destino = "Archivos/".$documentacion;
+    $ext=substr($documentacion, strrpos($documentacion, '.'));
+   if  (in_array($ext,$formato)){
     if ($documentacion != "") {
         if (copy($ruta, $destino))
             $conexion =new Conect_MySql();
@@ -24,19 +25,26 @@ $documentacion = $_FILES['rubricass']['name'];
 }
 }
 }
+}
 
 if (isset($_POST['Guardar'])){
 
-$equipo = $_POST["equipo"];
-$rubrica = $_POST["rubrica"];
+//$equipo = 1;
+//$rubrica = 1;
+$equipo = $_POST["id_Equipo"];
+$rubrica = $_POST["id_Documentacion"];
+//$insertar = "SELECT equipoeva.id_Equipo, documentacion.id_Documentacion
+//FROM  "
+//"INSERT INTO equipodocumentacion SELECT * FROM equipoeva where 
+//equipoeva.id_Equipo = equipodocumentacion.id_Equipo ";        
+  $insertar =  "INSERT INTO  equipodocumentacion (id_Equipo, id_Documentacion) VALUES ('$equipo','$rubrica')"; 
+  $conexion =new Conect_MySql();
+  $res = $conexion->execute($insertar);
 
-  $insertar =  "INSERT INTO  equido (equipo, rubrica) VALUES ('$equipo', '$rubrica')"; 
-   $resu = mysqli_query($conexion, $insertar);
-
-if(!$insertar){
+if(!$res){
   echo 'Error al registrar';
 }else{
   echo 'Registro correctoo';
 }
 }
-?>
+?>       
