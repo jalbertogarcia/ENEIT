612250d43ee5eac0f9b3ff444bc02d9a1cad787d
IRVING\irvin <chokocrazy9@gmail.com>
2017-07-11 20:48:01 -0500

    ENVIAR CORREO ELECTRONICO COMPLETO

--- controller.php
+++ controller.php
@@ -1,33 +1,44 @@
 <?php
+$Nombre = "Organizador";
+$Email = $_POST['Email'];
+$Mensaje = $_POST['Mensaje'];
 
-	//array de correos 
-	$emails = $_POST['correos'];
+if ($Nombre=='' || $Email=='' || $Mensaje==''){ // Si falta un dato en el formulario mandara una alerta al usuario.
 
+echo "<script>alert('Los campos marcados con * son obligatorios');location.href ='javascript:history.back()';</script>";
 
-	// obtengo el cantidad de valores que vienen por post
-	//para la condicion de un ciclo
-	//el ciclo se repetira ejecutando la funcion enviarEmail
-	//cambiando unicamente el parametro de email
-	$numeroEmails = count($emails); 
-	echo "Correos Enviados " . $numeroEmails;
-
-	for ($i=0; $i < $numeroEmails ; $i++) { 
-		enviarEmail($emails[$i]);
-	}
+}else{
 
 
-	function enviarEmail($destino){
-		$asunto = "comentario de prueba";
+    require("archivosformulario/class.phpmailer.php"); // Requiere PHPMAILER para poder enviar el formulario desde el SMTP de google
+    $mail = new PHPMailer();
 
-		
-		
-		$headers = 'From: '.$destino."\r\n".
-			'Reply-To:'.$destino."\r\n".
-			'X-Mailer: PHP/'.phpversion();
-		mail($destino, $asunto,  $headers);
+    $mail->From     = $Email;
+    $mail->FromName = $Nombre; 
+    $mail->AddAddress($Email); // Dirección a la que llegaran los mensajes.
 
-		echo "<br/><strong>Invitacion enviada a " . $destino . "</strong> <br> ";
+// Aquí van los datos que apareceran en el correo que reciba
 
-	}
+    $mail->WordWrap = 50; 
+    $mail->IsHTML(true);     
+    $mail->Subject  =  "Contacto"; // Asunto del mensaje.
+    $mail->Body     =  "Nombre: $Nombre \n<br />". // Nombre del usuario
+    "Email: $Email \n<br />".    // Email del usuario
+    "Mensaje: $Mensaje \n<br />"; // Mensaje del usuario
+
+// Datos del servidor SMTP, podemos usar el de Google, Outlook, etc...
+
+    $mail->IsSMTP(); 
+    $mail->Host = "ssl://smtp.gmail.com:465";  // Servidor de Salida. 465 es uno de los puertos que usa Google para su servidor SMTP
+    $mail->SMTPAuth = true; 
+    $mail->Username = "chokocrazy9@gmail.com";  // Correo Electrónico
+    $mail->Password = "IRVING9JORDAN9"; // Contraseña del correo
+
+    if ($mail->Send())
+    echo "<script>alert('Formulario enviado exitosamente, le responderemos lo más pronto posible.');location.href ='javascript:history.back()';</script>";
+    else
+    echo "<script>alert('Error al enviar el formulario');location.href ='javascript:history.back()';</script>";
+
+}
 
 ?>
